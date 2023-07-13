92de4b70c57e09f9a217d71675318f7cffa041a5
Oleksandr <oleksandr.huziuk@gmail.com>
2023-07-13 12:30:55 +0300

    Add favicon icon and some css files for project,creation of project architecture

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import './assets/styles/colors.css'
+import './assets/styles/fonts.css'
+import './assets/styles/reset.css'
+
+
+
+
 const app = createApp(App);
 
 app.mount('#app')
