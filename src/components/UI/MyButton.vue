44411dee3c74d6d76aa89daf67d54f0d157c296a
Oleksandr <oleksandr.huziuk@gmail.com>
2023-07-14 10:23:46 +0300

    Create UI component

---
+++ src/components/UI/MyButton.vue
@@ -0,0 +1,18 @@
+<template>
+    <button class="btn">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+    export default {
+        name: 'my-button'
+    }
+</script>
+
+<style scoped>
+
+.btn{
+    background-color: var(--button-background-color);
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -5,9 +5,15 @@ import './assets/styles/colors.css'
 import './assets/styles/fonts.css'
 import './assets/styles/reset.css'
 
+import components from './components/UI'
 
 
 
 const app = createApp(App);
+
+
+components.forEach(component => {
+    app.component(component.name, component)
+});
 
 app.mount('#app')
