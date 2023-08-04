20d802edd8cf1c392730f41ea7f63272dfa7b0a8
Oleksandr <oleksandr.huziuk@gmail.com>
2023-08-04 12:24:05 +0300

    FIX

--- src/components/Main/Trending/Trending.vue
+++ src/components/Main/Trending/Trending.vue
@@ -33,7 +33,7 @@ import axios from 'axios'
 
         async mounted() {
             axios
-                .get('https://marketplace-orcin-sigma.vercel.app/data/Collections.json')
+                .get('https://marketplace-orcin-sigma.vercel.app/Merketplace/data/Collections.json')
                 .then(response =>  {
                     this.collections = response.data.Collections;
                     console.log(this.collections);
